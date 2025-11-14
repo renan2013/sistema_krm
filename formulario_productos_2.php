7bcba3071e4e1b63f8cec84022657b29eebe380e
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-11-14 12:20:32 -0600

    a

--- formulario_productos_2.php
+++ formulario_productos_2.php
@@ -26,7 +26,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Cotización</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         body {
@@ -351,8 +351,8 @@ $result = $conn->query($sql);
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
 <?php
--- menu.php
+++ menu.php
@@ -1,3 +1,6 @@
+<?php
+$base_url = "https://renangalvan.net/sistema_krm/";
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -8,31 +11,31 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">KRM</a>
+        <a class="navbar-brand" href="<?php echo $base_url; ?>index.php">KRM</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Inicio<span class="sr-only">(actual)</span></a>
+                    <a class="nav-link" href="<?php echo $base_url; ?>index.php">Inicio<span class="sr-only">(actual)</span></a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="formulario_productos_2.php">Crear Orden de Compra<span class="sr-only">(actual)</span></a>
+                    <a class="nav-link" href="<?php echo $base_url; ?>formulario_productos_2.php">Crear Orden de Compra<span class="sr-only">(actual)</span></a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="cliente.php">Registrar Cliente</a>
+                    <a class="nav-link" href="<?php echo $base_url; ?>cliente.php">Registrar Cliente</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="lista_cotizaciones.php">Lista órdenes de compra</a>
+                    <a class="nav-link" href="<?php echo $base_url; ?>lista_cotizaciones.php">Lista órdenes de compra</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Gestionar
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="categoria.php">Categorías</a>
-                        <a class="dropdown-item" href="producto.php">Productos</a>
+                        <a class="dropdown-item" href="<?php echo $base_url; ?>categoria.php">Categorías</a>
+                        <a class="dropdown-item" href="<?php echo $base_url; ?>producto.php">Productos</a>
                     </div>
                 </li>
             </ul>
