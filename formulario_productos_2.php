1b9b88dac96ce79d06bb62180e87851173df832c
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-11-14 12:09:30 -0600

    a

--- formulario_productos_2.php
+++ formulario_productos_2.php
@@ -350,7 +350,7 @@ $result = $conn->query($sql);
         </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
